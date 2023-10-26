16f2bcf6e353ab064769bc07c6d310d31134618e
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-10-26 19:03:12 +0300

    create proj

---
+++ Airbnb-Demo/Airbnb-Demo/Airbnb_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_DemoApp.swift
+//  Airbnb-Demo
+//
+//  Created by Marcel on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
