a718d86fbdc4b5fa1172c0b832fba63c335a21a2
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-10-27 15:24:21 +0300

    Done Search component

--- Airbnb-Demo/Airbnb-Demo/Views/Explore/ExploreView.swift
+++ Airbnb-Demo/Airbnb-Demo/Views/Explore/ExploreView.swift
@@ -11,6 +11,8 @@ struct ExploreView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                SearchAndFilterBar()
+
                 /// Only `loads` what we can see on the screen
                 LazyVStack(spacing: 25) {
                     ForEach(0..<10, id: \.self) { item in
